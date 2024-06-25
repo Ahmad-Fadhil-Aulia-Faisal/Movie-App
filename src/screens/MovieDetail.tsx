1a4623cf6d03ce413e623472b7814a93dbd730ba
dekDika13 <kadekdikaramasuryadi@gmail.com>
2024-06-25 17:43:51 +0800

    api integration

--- src/screens/MovieDetail.tsx
+++ src/screens/MovieDetail.tsx
@@ -1,22 +1,43 @@
 import React from 'react'
-import { View, Text, StyleSheet, Button } from 'react-native'
+import { View, Text, Button } from 'react-native'
 
-export default function MovieDetail({navigation}:any): JSX.Element {
+const MovieDetail = ({ navigation }: any): any => {
+    const fetchData = (): void => {
+        // Gantilah dengan akses token Anda
+        const ACCESS_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MzI5ZWE2NzBlOTAzMDgzODRhMjc1N2MxNTI3YzNiZCIsIm5iZiI6MTcxOTMwNzk5NC43NjcwMjMsInN1YiI6IjY2N2E4YmQ3NGFmOTM1YTgwY2Y2OWE3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OXZXdWsk5imXuUF72CEtaFcexxF_BsB8GaPoLjP32FY'
+
+        const url =
+            'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1'
+
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: `Bearer ${ACCESS_TOKEN}`,
+            },
+        }
+
+        fetch(url, options)
+            .then(async (response) => await response.json())
+            .then((response) => {
+                console.log(response)
+            })
+            .catch((err) => {
+                console.error(err)
+            })
+    }
+
     return (
-        <View style={styles.container}>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text>Movie Detail Page</Text>
-            <Button 
-            title='Kembali'
-            onPress={()=>navigation.goBack()}
+            <Button
+                title="Fetch Data"
+                onPress={() => {
+                    fetchData()
+                }}
             />
         </View>
     )
 }
-const styles = StyleSheet.create({
-    container: {
-        flex:1,
-        alignItems: 'center',
-        justifyContent: 'center'
 
-    },
-})
+export default MovieDetail
