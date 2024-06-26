58db796510b4157080e5ee3c54de1e3a881228ae
yemimamds <sihmirahmds@gmail.com>
2024-06-26 08:56:57 +0700

    fitur favorite

--- src/screens/Favorite.tsx
+++ src/screens/Favorite.tsx
@@ -1,19 +1,88 @@
-import React from 'react'
-import { View, Text, StyleSheet  } from 'react-native'
+import React, { useCallback, useState, } from 'react';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, Image } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
+import type { Movie } from '../types/app';
 
-export default function Favorite(): JSX.Element {
+const Favorite = (): JSX.Element => {
+    const [favorites, setFavorites] = useState<Movie[]>([]);
+    const navigation = useNavigation();
+
+    const fetchFavorites = async (): Promise<void> => {
+        try {
+            const initialData: string | null = await AsyncStorage.getItem('@FavoriteList');
+            if (initialData != null) {
+                const parsedData: Movie[] = JSON.parse(initialData);
+                setFavorites(parsedData);
+            }
+        } catch (error) {
+            console.error('Error fetching favorites:', error);
+        }
+    };
+
+    useFocusEffect(
+        useCallback(() => {
+            fetchFavorites().catch(console.error);
+        }, [])
+    );
+
     return (
         <View style={styles.container}>
-            <Text>Favorite</Text>
+            <FlatList
+                data={favorites}
+                renderItem={({ item, index }) => (
+                    <TouchableOpacity
+                        onPress={() => {
+                            navigation.navigate('MovieDetail', { id: item.id });
+                        }}
+                    >
+                        <View style={styles.movieItem}>
+                            <Image
+                                source={{ uri: `https://image.tmdb.org/t/p/w500${item.poster_path}` }}
+                                style={styles.movieImage}
+                            />
+                            <Text style={styles.movieTitle}>{item.title}</Text>
+                        </View>
+                    </TouchableOpacity>
+                )}
+                keyExtractor={(item, index) => `${item.id}-${index}`} // Use a combination of id and index
+                showsVerticalScrollIndicator={false}
+            />
         </View>
-    )
-}
+    );
+};
 
 const styles = StyleSheet.create({
     container: {
-        flex:1,
-        alignItems: 'center',
-        justifyContent: 'center'
-
+        marginTop:'10%',
+        flex: 1,
+        padding: 16,
+        backgroundColor: '#fff',
     },
-})
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 16,
+    },
+    movieItem: {
+        position: 'relative',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 16,
+        alignItems: 'stretch'
+    },
+    movieImage: {
+        width: '20%',
+        height: 100,
+        borderRadius: 8,
+        marginRight: 16,
+    },
+    movieTitle: {
+        position: 'absolute',
+        bottom: 8,
+        left: 8,
+        color:'white',
+    },
+});
+
+export default Favorite;
--- src/screens/MovieDetail.tsx
+++ src/screens/MovieDetail.tsx
@@ -1,64 +1,127 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { View, Text, Image, StyleSheet, FlatList, ActivityIndicator, ScrollView, TouchableOpacity } from 'react-native';
 import { API_ACCESS_TOKEN } from '@env';
-import { useNavigation, StackActions, } from '@react-navigation/native';
-import type { Movie, Recommendation, } from '../types/app';
+import { useNavigation, StackActions, useFocusEffect } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { FontAwesome } from '@expo/vector-icons';
+import type { Movie, Recommendation } from '../types/app';
 
 const MovieDetail = ({ route }: any): JSX.Element => {
     const { id } = route.params;
     const [movie, setMovie] = useState<Movie | null>(null);
     const [loading, setLoading] = useState<boolean>(true);
     const [recommendations, setRecommendations] = useState<Recommendation[]>([]);
+    const [isFavorite, setIsFavorite] = useState<boolean>(false);
     const navigation = useNavigation();
 
+    const fetchMovieDetail = async (): Promise<void> => {
+        try {
+            const url = `https://api.themoviedb.org/3/movie/${id}`;
+            const options = {
+                method: 'GET',
+                headers: {
+                    accept: 'application/json',
+                    Authorization: `Bearer ${API_ACCESS_TOKEN}`,
+                },
+            };
+            const response = await fetch(url, options);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data: Movie = await response.json();
+            setMovie(data);
+        } catch (error) {
+            console.error('Error fetching movie details:', error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const fetchRecommendations = async (): Promise<void> => {
+        try {
+            const url = `https://api.themoviedb.org/3/movie/${id}/recommendations`;
+            const options = {
+                method: 'GET',
+                headers: {
+                    accept: 'application/json',
+                    Authorization: `Bearer ${API_ACCESS_TOKEN}`,
+                },
+            };
+            const response = await fetch(url, options);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            setRecommendations(data.results as Recommendation[]);
+        } catch (error) {
+            console.error('Error fetching recommendations:', error);
+        }
+    };
+
+    const checkIsFavorite = async (): Promise<void> => {
+        try {
+            const initialData: string | null = await AsyncStorage.getItem('@FavoriteList');
+            if (initialData != null) {
+                const favMovieList: Movie[] = JSON.parse(initialData);
+                setIsFavorite(favMovieList.some(movie => movie.id === id));
+            }
+        } catch (error) {
+            console.error('Error checking favorite status:', error);
+        }
+    };
+
     useEffect(() => {
-        const fetchMovieDetail = async (): Promise<void> => {
-            try {
-                const url = `https://api.themoviedb.org/3/movie/${id}`;
-                const options = {
-                    method: 'GET',
-                    headers: {
-                        accept: 'application/json',
-                        Authorization: `Bearer ${API_ACCESS_TOKEN}`,
-                    },
-                };
-                const response = await fetch(url, options);
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                const data: Movie = await response.json();
-                setMovie(data);
-            } catch (error) {
-                console.error('Error fetching movie details:', error);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        const fetchRecommendations = async (): Promise<void> => {
-            try {
-                const url = `https://api.themoviedb.org/3/movie/${id}/recommendations`;
-                const options = {
-                    method: 'GET',
-                    headers: {
-                        accept: 'application/json',
-                        Authorization: `Bearer ${API_ACCESS_TOKEN}`,
-                    },
-                };
-                const response = await fetch(url, options);
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                const data = await response.json();
-                setRecommendations(data.results as Recommendation[]);
-            } catch (error) {
-                console.error('Error fetching recommendations:', error);
-            }
-        };
-
         fetchMovieDetail().catch(console.error);
         fetchRecommendations().catch(console.error);
     }, [id]);
+
+    useFocusEffect(
+        useCallback(() => {
+            checkIsFavorite().catch(console.error);
+        }, [id])
+    );
+
+    const addFavorite = async (movie: Movie): Promise<void> => {
+        try {
+            const initialData: string | null = await AsyncStorage.getItem('@FavoriteList');
+            let favMovieList: Movie[] = [];
+
+            if (initialData !== null) {
+                favMovieList = [...JSON.parse(initialData), movie];
+            } else {
+                favMovieList = [movie];
+            }
+
+            await AsyncStorage.setItem('@FavoriteList', JSON.stringify(favMovieList));
+            setIsFavorite(true);
+        } catch (error) {
+            console.error('Error adding to favorites:', error);
+        }
+    };
+
+    const removeFavorite = async (id: number): Promise<void> => {
+        try {
+            const initialData: string | null = await AsyncStorage.getItem('@FavoriteList');
+            if (initialData != null) {
+                const favMovieList: Movie[] = JSON.parse(initialData);
+                const updatedFavMovieList = favMovieList.filter(movie => movie.id !== id);
+                await AsyncStorage.setItem('@FavoriteList', JSON.stringify(updatedFavMovieList));
+                setIsFavorite(false);
+            }
+        } catch (error) {
+            console.error('Error removing from favorites:', error);
+        }
+    };
+
+    const handleFavoriteToggle = (): void => {
+        if (movie != null) {
+            if (isFavorite) {
+                removeFavorite(movie.id).catch(console.error);
+            } else {
+                addFavorite(movie).catch(console.error);
+            }
+        }
+    };
 
     if (loading) {
         return (
@@ -79,29 +142,38 @@ const MovieDetail = ({ route }: any): JSX.Element => {
                                 style={styles.backdrop}
                             />
                             <View style={styles.overlay}>
+                                <View>
                                 <Text style={styles.title}>{movie.title}</Text>
                                 <Text style={styles.ratingText}>⭐{movie.vote_average}</Text>
+                                </View>
+                                <TouchableOpacity onPress={handleFavoriteToggle}>
+                                    <FontAwesome 
+                                        name={isFavorite ? 'heart' : 'heart-o'} 
+                                        size={24} 
+                                        color={isFavorite ? 'red' : 'white'} 
+                                    />
+                                </TouchableOpacity>
                             </View>
                         </View>
                         <Text style={styles.overview}>{movie.overview}</Text>
                         <View style={styles.info}>
                             <View style={styles.infoColumn}>
                                 <View style={styles.infoRow}>
-                                    <Text style={styles.infoLabel}>Original Language:</Text>
+                                    <Text style={styles.infoLabel}>Original Language</Text>
                                     <Text style={styles.infoValue}>{movie.original_language}</Text>
                                 </View>
                                 <View style={styles.infoRow}>
-                                    <Text style={styles.infoLabel}>Release Date:</Text>
+                                    <Text style={styles.infoLabel}>Release Date</Text>
                                     <Text style={styles.infoValue}>{movie.release_date.toString()}</Text>
                                 </View>
                             </View>
                             <View style={styles.infoColumn}>
                                 <View style={styles.infoRow}>
-                                    <Text style={styles.infoLabel}>Popularity:</Text>
+                                    <Text style={styles.infoLabel}>Popularity</Text>
                                     <Text style={styles.infoValue}>{movie.popularity}</Text>
                                 </View>
                                 <View style={styles.infoRow}>
-                                    <Text style={styles.infoLabel}>Vote Count:</Text>
+                                    <Text style={styles.infoLabel}>Vote Count</Text>
                                     <Text style={styles.infoValue}>{movie.vote_count}</Text>
                                 </View>
                             </View>
@@ -141,9 +213,13 @@ const MovieDetail = ({ route }: any): JSX.Element => {
     );
 };
 
+
 const styles = StyleSheet.create({
+    
     container: {
         padding: 16,
+        flex: 1,
+        backgroundColor: '#fff',
     },
     loadingContainer: {
         flex: 1,
@@ -155,13 +231,19 @@ const styles = StyleSheet.create({
     },
     backdrop: {
         width: '100%',
-        height: 200,
+        height: 250,
         borderRadius: 8,
     },
     overlay: {
         position: 'absolute',
-        bottom: 16,
-        left: 16,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        padding: 10,
     },
     title: {
         fontSize: 24,
@@ -241,7 +323,5 @@ const styles = StyleSheet.create({
         marginTop: '5%',
     },
 });
-
-
 
 export default MovieDetail;
